e72ff13222b3b5e55374090e90adff839be56726
Bartosz Hinc <baton1231@o2.pl>
2014-01-22 01:45:49 +0100

    Gotowe

---
+++ js/skrypt.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    'use strict';
+    var socket = io.connect('http://localhost:3000');
+
+     $( '#widz' ).click(function(){
+      	
+      		document.write('dupa');
+      });
+  
+});
